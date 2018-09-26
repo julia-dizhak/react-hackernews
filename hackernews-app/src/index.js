115fbb33233902c3265dac70215da1ffd04d0023
Yulia <yulia.scherbina@gmail.com>
2018-09-26 21:32:36 +0200

    add create-react-app

--- hackernews-app/src/index.js
+++ hackernews-app/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './static/css/index.css';
-
-import App from './static/jsx/Site';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 registerServiceWorker();
