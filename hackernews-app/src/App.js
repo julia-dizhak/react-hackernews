19735f354f78df55a71268278371661df668cc00
Yulia <yulia.scherbina@gmail.com>
2018-09-27 15:43:40 +0200

    update App component

--- hackernews-app/src/App.js
+++ hackernews-app/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+      
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MoneyPark</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
       </div>
     );
