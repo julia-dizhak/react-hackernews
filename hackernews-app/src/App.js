7ed0d1d493d9feef857bb8b8ec06f45507af347d
Yulia <yulia.scherbina@gmail.com>
2018-09-30 09:28:00 +0200

    v1.1 migrate to stateless function

--- hackernews-app/src/App.js
+++ hackernews-app/src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        const name = 'Julia Dizhak';
+export default function App(props) {
+    const name = 'Julia Dizhak';
+
+    return (
+        <div className="app">
+            <h1>
+                <span>{name}</span> love React   
+            </h1>     
+        </div>
         
-        return (
-            <div className="app">
-                <h1>
-                    Welcome to React &nbsp;
-                    <span>{name}</span>
-                </h1>     
-            </div>
-        );
-    }
+    )
 }
-
-export default App;
